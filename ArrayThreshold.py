97dd13a40774fbf5070af179beaef683879ee4fc
charanvarma390 <charan@CV64.>
2025-01-26 01:37:23 -0600

    Done Misc-6

--- ArrayThreshold.py
+++ ArrayThreshold.py
@@ -1,8 +1,9 @@
-#Time Complexity: O(n)
+#Time Complexity: O(nlogn + n)
 #Space Complexity: O(1)
 def findThreshold(nums,desiredsum):
     n = len(nums)
     threshold = desiredsum/n
+    nums.sort()
     for i,num in enumerate(nums):
         if(num<threshold):
             difference = threshold - num
